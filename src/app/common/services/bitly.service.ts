dfdefd1a19db064b8d249eb7fef8436363454400
Ezequiel <85073571+Ezequielp19@users.noreply.github.com>
2024-10-24 15:38:24 -0300

    Update bitly.service.ts

--- src/app/common/services/bitly.service.ts
+++ src/app/common/services/bitly.service.ts
@@ -1,26 +1,71 @@
-import { Injectable } from '@angular/core';
+// import { Injectable } from '@angular/core';
+// import { HttpClient, HttpHeaders } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class BitlyService {
+//   private apiUrl = 'https://api-ssl.bitly.com/v4/shorten';
+//   private accessToken = '240a3a26194de9c1916d6f88e5ed7ce6f320ba7a'; 
+
+//   constructor(private http: HttpClient) {}
+
+//   shortenUrl(longUrl: string): Observable<any> {
+//     const headers = new HttpHeaders({
+//       'Authorization': `Bearer ${this.accessToken}`,
+//       'Content-Type': 'application/json'
+//     });
+
+//     const body = {
+//       long_url: longUrl
+//     };
+
+//     return this.http.post(this.apiUrl, body, { headers });
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class BitlyService {
-  private apiUrl = 'https://api-ssl.bitly.com/v4/shorten';
-  private accessToken = '240a3a26194de9c1916d6f88e5ed7ce6f320ba7a'; // Asegúrate de manejar este token de forma segura
+export class UrlShortenerService {
+  private apiUrl = 'https://api.tinyurl.com/create';
+  private apiKey = 'nNRgzGIiQVp3NiT2clQMutn3pXSzhHb5vXMopOcK6E07lEM1OsUX0UmPgzgO';  
 
   constructor(private http: HttpClient) {}
 
-  shortenUrl(longUrl: string): Observable<any> {
+  async shortenQRCodeUrl(qrStorageUrl: string): Promise<string> {
     const headers = new HttpHeaders({
-      'Authorization': `Bearer ${this.accessToken}`,
+      'Authorization': `Bearer ${this.apiKey}`,
       'Content-Type': 'application/json'
     });
 
     const body = {
-      long_url: longUrl
+      url: qrStorageUrl,
+      domain: 'tiny.one'
     };
 
-    return this.http.post(this.apiUrl, body, { headers });
+    try {
+      const response: any = await this.http.post(this.apiUrl, body, { headers }).toPromise();
+      const shortenedUrl = response.data.tiny_url;
+      console.log('URL acortada:', shortenedUrl);
+      return shortenedUrl;
+    } catch (error) {
+      console.error('Error al acortar la URL:', error);
+      throw error;
+    }
   }
 }
+
