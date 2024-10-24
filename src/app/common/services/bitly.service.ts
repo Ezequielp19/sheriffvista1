85ec9d92256bccfec215bbb7902331f1d2b89479
Ezequiel <85073571+Ezequielp19@users.noreply.github.com>
2024-10-24 15:52:05 -0300

    Update bitly.service.ts

--- src/app/common/services/bitly.service.ts
+++ src/app/common/services/bitly.service.ts
@@ -1,37 +1,3 @@
-// import { Injectable } from '@angular/core';
-// import { HttpClient, HttpHeaders } from '@angular/common/http';
-// import { Observable } from 'rxjs';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class BitlyService {
-//   private apiUrl = 'https://api-ssl.bitly.com/v4/shorten';
-//   private accessToken = '240a3a26194de9c1916d6f88e5ed7ce6f320ba7a'; 
-
-//   constructor(private http: HttpClient) {}
-
-//   shortenUrl(longUrl: string): Observable<any> {
-//     const headers = new HttpHeaders({
-//       'Authorization': `Bearer ${this.accessToken}`,
-//       'Content-Type': 'application/json'
-//     });
-
-//     const body = {
-//       long_url: longUrl
-//     };
-
-//     return this.http.post(this.apiUrl, body, { headers });
-//   }
-// }
-
-
-
-
-
-
-
-
 
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -42,26 +8,25 @@ import { Injectable } from '@angular/core';
 })
 export class UrlShortenerService {
   private apiUrl = 'https://api.tinyurl.com/create';
-  private apiKey = 'nNRgzGIiQVp3NiT2clQMutn3pXSzhHb5vXMopOcK6E07lEM1OsUX0UmPgzgO';  
+  private apiKey = 'nNRgzGIiQVp3NiT2clQMutn3pXSzhHb5vXMopOcK6E07lEM1OsUX0UmPgzgO';
 
   constructor(private http: HttpClient) {}
 
-  async shortenQRCodeUrl(qrStorageUrl: string): Promise<string> {
+   // Método para acortar cualquier URL
+  async shortenUrl(longUrl: string): Promise<string> {
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${this.apiKey}`,
       'Content-Type': 'application/json'
     });
 
     const body = {
-      url: qrStorageUrl,
+      url: longUrl,
       domain: 'tiny.one'
     };
 
     try {
       const response: any = await this.http.post(this.apiUrl, body, { headers }).toPromise();
-      const shortenedUrl = response.data.tiny_url;
-      console.log('URL acortada:', shortenedUrl);
-      return shortenedUrl;
+      return response.data.tiny_url; // Devuelve la URL acortada
     } catch (error) {
       console.error('Error al acortar la URL:', error);
       throw error;
